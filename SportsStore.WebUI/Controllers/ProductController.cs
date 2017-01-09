894911aa5bfc762fdd3d263336b6c411f22b0e37
mahyak <m.kardaani@gmail.com>
2017-01-09 11:57:26 +0330

    SportsStore

---
+++ SportsStore.WebUI/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using SportsStore.Domain.Abstract;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductsRepository repository;
+        public ProductController(IProductsRepository ProductRepository)
+        {
+            this.repository = ProductRepository;
+        }
+        public ViewResult List()
+        {
+            return View(repository.Products);
+        }
+    }
+}
---
+++ SportsStore.WebUI/InfraStructure/NinjectDependencyResolver.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+using Ninject;
+using SportsStore.WebUI.Infrastructure;
+using Moq;
+using System.Linq;
+using SportsStore.Domain.Abstract;
+using SportsStore.Domain.Entities;
+using SportsStore.Domain.Concrete;
+
+namespace SportsStore.WebUI.Infrastructure
+{
+    public class NinjectDependencyResolver : IDependencyResolver
+    {
+        private IKernel kernel;
+        public NinjectDependencyResolver(IKernel kernelParam)
+        {
+            kernel = kernelParam;
+            AddBindings();
+        }
+        public object GetService(Type serviceType)
+        {
+            return kernel.TryGet(serviceType);
+        }
+        public IEnumerable<object> GetServices(Type serviceType)
+        {
+            return kernel.GetAll(serviceType);
+        }
+        private void AddBindings()
+        {
+            kernel.Bind<IProductsRepository>().To<EFProductRepository>();
+        }
+    }
+}
