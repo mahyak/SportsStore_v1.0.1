58f6248b8863bbfa37eeaa371dc4409539fad582
mahyak <m.kardaani@gmail.com>
2017-01-09 12:45:48 +0330

    SportsStore

--- SportsStore.UnitTests/App_Start/NinjectWebCommon.cs
+++ SportsStore.UnitTests/App_Start/NinjectWebCommon.cs
@@ -5,26 +5,28 @@ namespace SportsStore.UnitTests.App_Start
 {
     using System;
     using System.Web;
-
     using Microsoft.Web.Infrastructure.DynamicModuleHelper;
-
     using Ninject;
     using Ninject.Web.Common;
+    using Ninject.Web.Mvc;
+    
 
-    public static class NinjectWebCommon 
+
+
+    public static class NinjectWebCommon
     {
         private static readonly Bootstrapper bootstrapper = new Bootstrapper();
 
         /// <summary>
         /// Starts the application
         /// </summary>
-        public static void Start() 
+        public static void Start()
         {
             DynamicModuleUtility.RegisterModule(typeof(OnePerRequestHttpModule));
             DynamicModuleUtility.RegisterModule(typeof(NinjectHttpModule));
             bootstrapper.Initialize(CreateKernel);
         }
-        
+
         /// <summary>
         /// Stops the application.
         /// </summary>
@@ -32,7 +34,7 @@ namespace SportsStore.UnitTests.App_Start
         {
             bootstrapper.ShutDown();
         }
-        
+
         /// <summary>
         /// Creates the kernel that will manage your application.
         /// </summary>
@@ -42,7 +44,7 @@ namespace SportsStore.UnitTests.App_Start
             var kernel = new StandardKernel();
             kernel.Bind<Func<IKernel>>().ToMethod(ctx => () => new Bootstrapper().Kernel);
             kernel.Bind<IHttpModule>().To<HttpApplicationInitializationHttpModule>();
-            
+
             RegisterServices(kernel);
             return kernel;
         }
@@ -53,8 +55,7 @@ namespace SportsStore.UnitTests.App_Start
         /// <param name="kernel">The kernel.</param>
         private static void RegisterServices(IKernel kernel)
         {
-            System.Web.Mvc.DependencyResolver.SetResolver(new
-            SportsStore.WebUI.Infrastructure.NinjectDependencyResolver(kernel));
-        }        
+            System.Web.Mvc.DependencyResolver.SetResolver(new NinjectDependencyResolver(kernel));
+        }
     }
 }
